c1367d2fb772c97b19daf3c7d344f17ddf8edffd
tfavro <wetr9902@gmail.com>
2022-01-21 15:19:32 -0600

    created html and modules

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const fs = require('fs');
+const generatePage = require('./src/page-template');
+const profileDataArgs = process.argv.slice(2);
+const [names, github] = profileDataArgs;
+
+
+//or it can look like this:
+//const name = profileDataArgs[0];
+//const github = profileDataArgs[1];
+
+
+/*console.log(profileDataArgs);
 
 const printProfileData = profileDataArr => {
     //This...
@@ -16,4 +26,12 @@ const printProfileData = profileDataArr => {
     profileDataArr.forEach(profileItem => console.log(profileItem));
 };
 
-printProfileData(profileDataArgs);
+printProfileData(profileDataArgs);*/
+
+
+
+fs.writeFile('index.html', generatePage(names, github), err =>{
+    if (err) throw new Error(err);
+    
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
