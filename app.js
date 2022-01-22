9e55b16fd45fd631545552e29ab4ba1982f18289
tfavro <wetr9902@gmail.com>
2022-01-21 18:32:03 -0600

    added npm inquirer

--- app.js
+++ app.js
@@ -1,37 +1,21 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template');
-const profileDataArgs = process.argv.slice(2);
-const [names, github] = profileDataArgs;
+const inquirer = require('inquirer');
+//const fs = require('fs');
+//const generatePage = require('./src/page-template');
 
+// const pageHTML = generatePage(names, github);
 
-//or it can look like this:
-//const name = profileDataArgs[0];
-//const github = profileDataArgs[1];
-
-
-/*console.log(profileDataArgs);
-
-const printProfileData = profileDataArr => {
-    //This...
-    for(let i = 0; i<profileDataArr.length; i++){
-        console.log(profileDataArr[i]);
-    }
-    console.log('===============');
-    //is the same as this...
-    profileDataArr.forEach((profileItem)=>{
-        console.log(profileItem)
-    });
-    console.log('================');
-    //is the same as this....
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
-
-printProfileData(profileDataArgs);*/
-
-
-
-fs.writeFile('index.html', generatePage(names, github), err =>{
-    if (err) throw new Error(err);
+// fs.writeFile('./index.html', pageHTML, err =>{
+//     if (err) throw new Error(err);
     
-    console.log('Portfolio complete! Check out index.html to see the output!');
-});
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
