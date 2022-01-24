06bd930f3400837b8775307222edf5a78a9a292f
tfavro <wetr9902@gmail.com>
2022-01-23 20:21:06 -0600

    added sections to the html

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
-//const fs = require('fs');
-//const generatePage = require('./src/page-template');
+const fs = require('fs');
+const generatePage = require('./src/page-template');
 
 // const pageHTML = generatePage(names, github);
 
@@ -140,8 +140,14 @@ const promptProject = portfolioData =>{
     });  
 }
 promptUser()
-.then(promptProject)
-.then(portfolioData =>{
-    console.log(portfolioData);
+    .then(promptProject)
+    .then(portfolioData => {
+        const pageHTML = generatePage(portfolioData);
+
+        fs.writeFile('./index.html', pageHTML, err => {
+           if (err) throw new Error(err);
+
+           console.log('Page created! Check out index.html in this directory to see it!');
+        });
 });
 
