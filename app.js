39e2969889ebfae14df94256894f29c0b956530b
tfavro <wetr9902@gmail.com>
2022-01-24 09:48:44 -0600

    generate the website

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
+const {writeFile, copyFile} = require('./utils/generate-site.js');
 const inquirer = require('inquirer');
-const fs = require('fs');
 const generatePage = require('./src/page-template');
 
 // const pageHTML = generatePage(names, github);
 
-// fs.writeFile('./index.html', pageHTML, err =>{
+// fs.writeFile('./dist/index.html', pageHTML, err =>{
 //     if (err) throw new Error(err);
     
 //     console.log('Portfolio complete! Check out index.html to see the output!');
@@ -142,12 +142,20 @@ const promptProject = portfolioData =>{
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        const pageHTML = generatePage(portfolioData);
-
-        fs.writeFile('./index.html', pageHTML, err => {
-           if (err) throw new Error(err);
-
-           console.log('Page created! Check out index.html in this directory to see it!');
-        });
-});
+        return generatePage(portfolioData);
+    })
+    .then(pageHTML => {
+        return writeFile(pageHTML);
+    })
+    .then(writeFileResponse => {
+        console.log(writeFileResponse);
+        return copyFile();
+    })
+    .then(copyFileResponse => {
+        console.log(copyFileResponse);
+    })
+    .catch(err => {
+        console.log(err);
+    });
+        
 
